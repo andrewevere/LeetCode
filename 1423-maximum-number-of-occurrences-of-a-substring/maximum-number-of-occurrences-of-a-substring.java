145294c7bba6e510557ae45ad22254ca2c46872d
andrewevere <117464487+andrewevere@users.noreply.github.com>
2025-02-08 00:57:50 -0800

    Time: 31 ms (95.54%) | Memory: 46.3 MB (87.65%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -1,30 +1,41 @@
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        int[] letter = new int[128];
-        Map<String, Integer> stringCnt = new HashMap<>();
-        int ans = 0;
+        int n = s.length();
+        Map<String, Integer> freqMap = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i ++){
-            letter[s.charAt(i)]++;
-            if (i < minSize - 1){
-                continue;
+        int[] freq = new int[128];
+        int distinct = 0;
+
+        for (int i = 0; i < minSize; i++){
+            char c = s.charAt(i);
+            if (freq[c] == 0){
+                distinct++;
             }
-            int distinctCnt = 0;
-            for (int element : letter){
-                if (element > 0){
-                    distinctCnt ++;
-                }
-            }
-            if (distinctCnt <= maxLetters){
-                String subStr = s.substring(i - minSize + 1, i + 1);
-                stringCnt.put(subStr, stringCnt.getOrDefault(subStr, 0) + 1);
-            }
-            Character out = s.charAt(i - minSize + 1);
-            letter[out]--;
+            freq[c]++;
         }
-        for (Map.Entry<String, Integer> entry : stringCnt.entrySet()) {
-            if (entry.getValue() > ans){
-                ans = entry.getValue();
+        if (distinct <= maxLetters){
+            String sub = s.substring(0, minSize);
+            freqMap.put(sub, 1);
+        }
+
+        int ans = freqMap.getOrDefault(s.substring(0, minSize), 0);
+        for (int i = minSize; i < n; i++){
+            char in = s.charAt(i);
+            if (freq[in] == 0){
+                distinct++;
+            }
+            freq[in]++;
+
+            char out = s.charAt(i - minSize);
+            freq[out]--;
+            if (freq[out] == 0){
+                distinct--;
+            }
+            if (distinct <= maxLetters){
+                String sub = s.substring(i - minSize + 1, i + 1);
+                int count = freqMap.getOrDefault(sub, 0) + 1;
+                freqMap.put(sub, count);
+                ans = Math.max(ans, count);
             }
         }
         return ans;
