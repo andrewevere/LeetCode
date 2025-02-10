cccf5d7822fa6736cf5b0f45c9789dd58108509b
andrewevere <117464487+andrewevere@users.noreply.github.com>
2025-02-10 12:21:51 -0800

    Time: 89 ms (5.69%) | Memory: 45.8 MB (24.80%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,39 +1,41 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        StringBuilder p1 = new StringBuilder(), p2 = new StringBuilder();
-        //拼接两份s, 模拟任意旋转后的情况
-        String s2 = s + s;
-        for (int i = 0; i < 2 * n; i++){
-            //对于p1, 偶数位'0', 奇数位'1'
-            //对于p2, 偶数位'1', 奇数位'0'
-            if (i % 2 == 0){
-                p1.append('0');
-                p2.append('1');
-            }else{
-                p1.append('1');
-                p2.append('0');
+        s = s + s;
+        StringBuilder alt1 = new StringBuilder();
+        StringBuilder alt2 = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            alt1.append(i % 2 == 0 ? "0" : "1");
+            alt2.append(i % 2 == 0 ? "1" : "0");
+        }
+
+        int res = s.length();
+        int diff1 = 0;
+        int diff2 = 0;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            if (s.charAt(r) != alt1.charAt(r)) {
+                diff1++;
+            }
+            if (s.charAt(r) != alt2.charAt(r)) {
+                diff2++;
+            }
+
+            if ((r - l + 1) > n) {
+                if (s.charAt(l) != alt1.charAt(l)) {
+                    diff1--;
+                }
+                if (s.charAt(l) != alt2.charAt(l)) {
+                    diff2--;
+                }
+                l++;
+            }
+
+            if ((r - l + 1) == n) {
+                res = Math.min(res, Math.min(diff1, diff2));
             }
         }
-        String pattern1 = p1.toString();
-        String pattern2 = p2.toString();
 
-        //构造前缀和数组，ps[i] 表示s2[0..i-1]与pattern1 的不同字符数总和
-        int[] ps1 = new int[2 * n + 1];
-        int[] ps2 = new int[2 * n + 1];
-        for (int i = 0; i < 2 * n; i++){
-            ps1[i + 1] = ps1[i] + (s2.charAt(i) == pattern1.charAt(i) ? 0 : 1);
-            ps2[i + 1] = ps2[i] + (s2.charAt(i) == pattern2.charAt(i) ? 0 : 1);
-        }
-
-        int ans = Integer.MAX_VALUE;
-        //枚举所有起点i, 敞口长度为 n
-        for (int i = 0; i < n; i++){
-            //在 [i, i+n) 区间内与两种模式的不同字符数量
-            int diff1 = ps1[i + n] - ps1[i];
-            int diff2 = ps2[i + n] - ps2[i];
-            ans = Math.min(ans, Math.min(diff1, diff2));
-        }
-        return ans;
+        return res;
     }
 }
